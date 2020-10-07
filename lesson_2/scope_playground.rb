691486719c6165ad1deeb3c485c6ca1d8d37dfab
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-10-07 18:05:21 -0400

    Add scope_playground

---
+++ lesson_2/scope_playground.rb
@@ -0,0 +1,32 @@
+# var1 = 456
+# loop do
+#   var1 = 123
+#   break
+# end
+
+# def a_method(var)
+#   var += 1
+# end
+
+# puts var1
+
+# var1 = a_method(var1)
+
+# puts var1
+
+# def change_name(name)
+#   name = 'bob'
+#   puts name
+# end
+
+# name = 'jim'
+# change_name(name)
+# puts name
+
+# def a_method(str)
+#   str.capitalize!
+# end
+
+# my_name = 'benjamin'
+# a_method(my_name)
+# puts my_name
